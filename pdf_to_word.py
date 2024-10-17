eb1cd7331a870186189908eed05198745ee10c9a
Aravind M <aravindm@dadoshealth.com>
2024-10-17 15:43:28 +0530

    Add changes

--- pdf_to_word.py
+++ pdf_to_word.py
@@ -1,5 +1,5 @@
 from pdf2docx import Converter
-
+import fitz  
 def pdf_to_word(pdf_file, docx_file):
     # Create a Converter object
     cv = Converter(pdf_file)
@@ -8,8 +8,28 @@ def pdf_to_word(pdf_file, docx_file):
     # Close the Converter object
     cv.close()
 
+def pdf_to_text(pdf_file, output_txt_file):
+    # Open the PDF file
+    document = fitz.open(pdf_file)
+    # Initialize a variable to store the extracted text
+    text = ""
+
+    # Loop through each page
+    for page_num in range(document.page_count):
+        page = document[page_num]
+        text += page.get_text("text")
+
+    # Save the extracted text to a file
+    with open(output_txt_file, 'w', encoding='utf-8') as f:
+        f.write(text)
+
+    # Close the PDF document
+    document.close()
+
 # Example usage
 pdf_file = 'sample.pdf'  # Path to your PDF file
 docx_file = 'Output_files/output_word.docx'  # Path to save the DOCX file
-
+text_file = 'Output_files/output_word.txt'
 pdf_to_word(pdf_file, docx_file)
+
+pdf_to_text(pdf_file, text_file)
